bdd362c92af207ad36da54384d408d5de3bb2100
lisperey <pilrss16@gmail.com>
2023-09-04 22:47:45 -0300

    Iniciando

---
+++ src/main/java/com/br/distribuidora/ajax/DistribuidoraAjaxApplication.java
@@ -0,0 +1,13 @@
+package com.br.distribuidora.ajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistribuidoraAjaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistribuidoraAjaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/distribuidora/ajax/DistribuidoraAjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.distribuidora.ajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistribuidoraAjaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
